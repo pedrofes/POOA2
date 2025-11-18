21358332349f40a194b053bff7e83a4344dff321
Pedro Fonseca <pedrofonseca2012@uol.com.br>
2025-11-17 22:25:31 -0300

    Atualizações

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from modelos.cardapio.bebida import Bebida
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5, 'Grande')
 prato_paozinho = Prato('Pãozinho', 2, 'O melhor pão da cidade')
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
